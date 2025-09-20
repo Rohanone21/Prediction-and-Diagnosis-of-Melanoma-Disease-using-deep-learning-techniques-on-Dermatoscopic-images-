d0ddbc83f7efb42089bcfbd63f046f248f55210d
Rohanone21 <139203094+Rohanone21@users.noreply.github.com>
2025-09-20 23:52:02 +0530

    Update ReportGeneration.py

--- ReportGeneration.py
+++ ReportGeneration.py
@@ -12,7 +12,7 @@ import os
 
 # Email credentials
 sender_email = "rohanphadtare134@gmail.com"
-sender_password = "jtmu eyyp nckd mbjp"  # Gmail App Password
+sender_password =  # Gmail App Password
 
 # Create a Tkinter window for user input
 Tk().withdraw()
@@ -160,3 +160,4 @@ def send_email(recipient, pdf_file):
 
 # Call the function to send the email
 send_email(recipient_email, pdf_filename)
+
